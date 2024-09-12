8d741f2f22be6d2f51cc61b950d4b1a2809f3714
Vladimir Shumkov <augusthoffman3@gmail.com>
2024-09-12 14:12:11 +0500

    another test

--- src/main/java/org/example/one.java
+++ src/main/java/org/example/one.java
@@ -1,5 +1,6 @@
 public class one {
     public static void main(String[] args) {
         two.greeting();
+
     }
 }
--- src/main/java/org/example/two.java
+++ src/main/java/org/example/two.java
@@ -1,5 +1,5 @@
 public class two {
     public static void greeting() {
-        System.out.println("Hello world!");
+        System.out.println("hello from two");
     }
 }
